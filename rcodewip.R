0a947067e4df32a9149e74fb19c48620a97cb575
Dominic Tabeta <dominic.tabeta@gmail.com>
2016-04-22 15:53:11 +1000

    completed R code, now ready to add into Rmd file

--- rcodewip.R
+++ rcodewip.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(lattice)
 
 # Loading the data into a dataframe 'activity'
 activity<-read.csv("activity.csv")
@@ -31,8 +32,55 @@ with(avgdailyactivity,plot(interval,avgstep,type="l"))
 # Find the interval with the highest average activity
 avgdailyactivity[which.max(avgdailyactivity$avgstep),]
 
+# Calculate and report the total number of missing values in the dataset
 
+totalna<-sum(is.na(activity$steps))
 
+# The strategy I choose to impute the missing values is to replace the NAs with the mean for that 5-minute interval
 
+imputedactivity<-activity
+for (i in 1:nrow(imputedactivity)) {
+        if (is.na(imputedactivity[i,"steps"])) {
+                imputedactivity[i,"steps"]<-avgdailyactivity[avgdailyactivity$interval==imputedactivity[i,"interval"],"avgstep"]
+                
+        } 
+}
 
+groupbydateimputed<-group_by(imputedactivity,date)
+
+# Calculate the total steps taken per day
+imputedstepsperday<-summarize(groupbydateimputed,sumsteps=sum(steps))
+imputedstepsperday
+
+# Plot the steps taken per day
+with(imputedstepsperday,plot(date,sumsteps,type="h"))
+
+# Calculate the mean number of steps taken per day
+mean(imputedstepsperday$sumsteps,na.rm=TRUE)
+
+# Calculate the median number of steps taken per day
+median(imputedstepsperday$sumsteps,na.rm=TRUE)
+
+# The effect of imputing the missing values has resulted in the mean steps per day
+# remaining the same and the median steps per day only increasing slightly from 10765
+# to 10766.19
+# The mean and median steps per day for the dataset set with imputed values are both the same
+# 
+
+# Applying the weekday/weekend labels to the dataset
+
+daylabeled<-mutate(imputedactivity,daylabel=weekdays(date))
+for (i in 1:nrow(daylabeled)) {
+        if (daylabeled[i,"daylabel"] %in% c("Saturday","Sunday")) {
+                daylabeled[i,"daylabel"]<-"weekend"
+        } else {
+                daylabeled[i,"daylabel"]<-"weekday"
+        }
+}
+
+groupbydaylabel<-group_by(daylabeled,daylabel,interval)
+weekdaycompare<-summarize(groupbydaylabel,avgstep=mean(steps))
+
+# Plotting the weekend vs weekend comparison of avg steps for each interval
+xyplot(avgstep~interval | factor(daylabel),data=weekdaycompare,groups=daylabel,type="l",xlab = "Interval",ylab="Number of steps",layout=c(1,2),col=c("blue","blue"),scales=list())
 
